8317dcb308616fe0973e7f79dbd3a245911b74dc
shtjrgus010 <shtjrgus010@naver.com>
2020-08-30 01:58:27 +0900

    home join login User profile Controller
    controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,27 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+
+  if (password != password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+  }
+};
+export const getLogin = (req, res) => {
+  res.render("Login", { pageTitle: "Log In" });
+};
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
 export const logout = (req, res) => res.render("Logout");
 export const users = (req, res) => res.render("Users");
 export const userDetail = (req, res) => res.render("User Detail");
