fda703f449f322d868f66f1ea574150529dcd81d
shtjrgus010 <shtjrgus010@naver.com>
2020-08-29 15:27:55 +0900

    search
    s

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -7,11 +7,12 @@ import {
   editVideo,
   deleteVideo,
 } from "../controllers/videoController";
+
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
 export default videoRouter;
